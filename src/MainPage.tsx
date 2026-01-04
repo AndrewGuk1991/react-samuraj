181d37c448e49db81d74199bae375f27400b2b31
andrew_guk <gukandrey91@gmail.com>
2026-01-04 21:31:44 +0300

    add title

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -13,6 +13,7 @@ export function MainPage() {
         <div>
 
             <div style={{display: 'flex', gap: '30px', margin: '20px'}}>
+                <h1>Musicfun Player</h1>
                 <TrackList
                     selectedTrackId={trackId}
                     onTrackSelect={handleTrackSelect} />
